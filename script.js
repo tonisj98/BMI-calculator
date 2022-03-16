ddfda91de61a23462573acd3c105be77d1a626ee
tonisj98 <antonio.serjime@gmail.com>
2022-03-16 13:39:37 +0100

    hola

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 // Implementar el código aqui
 
+
+const form = document.querySelector('form');
+console.log(form)
+
 // Escuchar el evento 'submit'
+
+form.addEventListener('submit', function(e){
+
+const height = document.querySelector("#height").value
+const weight = document.querySelector("#weight").value
+
+height = height *100
+height = height * height
+let resultado = weight / height
+
+let mensaje = document.getElementById("mensaje").textContent
+document.mensaje.textContent = resultado
+
+
+
+});
